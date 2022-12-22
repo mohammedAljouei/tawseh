1b835f8412edb6031e0277fd0e691d94ca501652
Mohammed Al-jouei <Mjouei@kafalah.gov.sa>
2022-12-22 10:15:08 +0300

    Day 2

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -32,7 +38,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MapSample(),
+      home: const Login(),
     );
   }
 }
@@ -40,30 +46,21 @@ class MyApp extends StatelessWidget {
 class Login extends StatelessWidget {
   const Login({super.key});
 
-  Future<void> authSetUp() async {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    print('ooo');
-    await FirebaseAuth.instance.verifyPhoneNumber(
-      phoneNumber: '+966 53 103 5146',
-      verificationCompleted: (PhoneAuthCredential credential) {
-        print(credential);
-      },
-      verificationFailed: (FirebaseAuthException e) {
-        print(e);
-        if (e.code == 'invalid-phone-number') {
-          print('The provided phone number is not valid gggggggggggg.');
-        }
-      },
-      codeSent: (String verificationId, int? resendToken) {},
-      codeAutoRetrievalTimeout: (String verificationId) {},
-    );
+  Future<void> addUser(String username) async {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    users.add({
+      'username': username,
+    }).then((value) => print(value.id));
+
+    users.get();
+
+    print('test');
   }
 
   @override
   Widget build(BuildContext context) {
-    authSetUp();
+    addUser('yyu');
     return Scaffold(
       appBar: AppBar(
         title: const Text('مرحبا'),
@@ -73,7 +70,7 @@ class Login extends StatelessWidget {
           Container(
             height: 150.0,
             width: 190.0,
-            padding: EdgeInsets.only(top: 40),
+            padding: const EdgeInsets.only(top: 40),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(200),
             ),
@@ -111,133 +108,51 @@ class Login extends StatelessWidget {
   }
 }
 
-// not working
-class MapSample extends StatefulWidget {
-  const MapSample({Key? key}) : super(key: key);
 
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
 
-class MapSampleState extends State<MapSample> {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.satellite,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
-      ),
-    );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
-}
-
-//OLD CODE EXAMPLE OF STATE FUL 
-//
-//
-//
-//
-//
-//
-//
-//class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
+// class MapSample extends StatefulWidget {
+//   const MapSample({Key? key}) : super(key: key);
 
 //   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
+//   State<MapSample> createState() => MapSampleState();
 // }
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
+// class MapSampleState extends State<MapSample> {
+//   final Completer<GoogleMapController> _controller =
+//       Completer<GoogleMapController>();
 
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
+//   static const CameraPosition _kGooglePlex = CameraPosition(
+//     target: LatLng(37.42796133580664, -122.085749655962),
+//     zoom: 14.4746,
+//   );
+
+//   static const CameraPosition _kLake = CameraPosition(
+//       bearing: 192.8334901395799,
+//       target: LatLng(37.43296265331129, -122.08832357078792),
+//       tilt: 59.440717697143555,
+//       zoom: 19.151926040649414);
 
 //   @override
 //   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
 //     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
+//       body: GoogleMap(
+//         mapType: MapType.hybrid,
+//         initialCameraPosition: _kGooglePlex,
+//         onMapCreated: (GoogleMapController controller) {
+//           _controller.complete(controller);
+//         },
 //       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
+//       floatingActionButton: FloatingActionButton.extended(
+//         onPressed: _goToTheLake,
+//         label: const Text('To the lake!'),
+//         icon: const Icon(Icons.directions_boat),
 //       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
 //     );
+//   }
+
+//   Future<void> _goToTheLake() async {
+//     final GoogleMapController controller = await _controller.future;
+//     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
 //   }
 // }
