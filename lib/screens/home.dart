6ce99b5ddc226a12747e987a84db48c7122203e3
Mohammed Al-jouei <Mjouei@kafalah.gov.sa>
2022-12-25 17:06:28 +0300

    Day 5, open draw io

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import '../firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -14,6 +15,26 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class Home extends StatelessWidget {
   Home({super.key});
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://tawseh-1.web.app'));
 
   @override
   Widget build(BuildContext context) {
@@ -28,21 +49,27 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text('مرحبا'),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 150.0,
-            width: 190.0,
-            padding: const EdgeInsets.only(top: 40),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(200),
-            ),
-            child: Center(
-              child: Image.asset('assets/images/i1.jpg'),
-            ),
-          )
-        ],
-      ),
+      body: WebViewWidget(controller: controller),
+      // body: Column(
+      //   children: [
+      //     Container(
+      //       height: 150.0,
+      //       width: 190.0,
+      //       padding: const EdgeInsets.only(top: 40),
+      //       decoration: BoxDecoration(
+      //         borderRadius: BorderRadius.circular(200),
+      //       ),
+      //       // child: Center(
+      //       //   child: Image.asset('assets/images/i1.jpg'),
+      //       // ),
+      //       // child: const GoogleMap(
+      //       //   initialCameraPosition: CameraPosition(
+      //       //       target: LatLng(37.4, -122.08), zoom: 14.4746),
+      //       // ))
+      //       child: WebViewWidget(controller: controller),
+      //     )
+      //   ],
+      // ),
     );
   }
 }
